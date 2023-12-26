2f34b529c5e0d164493a108022e00f2a8a644186
KSEN0X <snezhka1303@gmail.com>
2023-12-26 16:35:58 +0200

    hw-10

---
+++ src/js/1-timer.js
@@ -0,0 +1,130 @@
+'use strict';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector('[data-start]');
+buttonStart.disabled = true;
+
+const daysTimer = document.querySelector('[data-days]');
+const hoursTimer = document.querySelector('[data-hours]');
+const minutesTimer = document.querySelector('[data-minutes]');
+const secondsTimer = document.querySelector('[data-seconds]');
+
+let userSelectedDate = '';
+
+/**
+  |============================
+  | library flatpickr
+  |============================
+*/
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    // перевірка дати на майбутність
+    if (selectedDates[0] < new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        messageColor: '#FAFAFB',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+        closeOnEscape: true,
+        timeout: 5000,
+      });
+    } else {
+      buttonStart.disabled = false;
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+const datePicker = flatpickr(input, options);
+
+/**
+  |============================
+  | even
+  |============================
+*/
+input.addEventListener('focus', () => {
+  datePicker.config.defaultDate = new Date();
+});
+
+let intervalId;
+
+/**
+  |============================
+  | Start the countdown
+  |============================
+*/
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+
+  const currentTime = new Date().getTime();
+  const selectedTime = userSelectedDate.getTime();
+  let countdownInterval = selectedTime - currentTime;
+
+  /**
+    |============================
+    | timer
+    |============================
+  */
+  const time = convertMs(countdownInterval);
+  updateTimerDisplay(time);
+
+  intervalId = setInterval(() => {
+    countdownInterval -= 1000;
+    const updatedTime = convertMs(countdownInterval);
+    updateTimerDisplay(updatedTime);
+
+    if (countdownInterval <= 1000) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+
+  /**
+   |============================
+   | remove input
+   |============================
+ */
+  input.value = '';
+});
+
+// оновлення відображення таймера
+function updateTimerDisplay(time) {
+  daysTimer.textContent = addLeadingZero(time.days);
+  hoursTimer.textContent = addLeadingZero(time.hours);
+  minutesTimer.textContent = addLeadingZero(time.minutes);
+  secondsTimer.textContent = addLeadingZero(time.seconds);
+}
+
+/**
+  |============================
+  | update the timer display
+  |============================
+*/
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
